39e4a0c1e2e8236012e2632c297fb1eb6fd5774e
andrawan <andrawan.ep@gmail.com>
2021-01-21 17:12:06 +0700

    a

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <!-- <div id="nav">
       <router-link to="/maps">Maps</router-link> |
       <router-link to="/chat">Chat</router-link>
-    </div>
+    </div> -->
     <router-view />
   </div>
 </template>
